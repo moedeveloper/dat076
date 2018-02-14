c8ce8a45201eee0c935b14c3e061376e877fa760
Mohamad Mortada <mohamad.mortada@cybercom.com>
2018-02-14 18:59:00 +0100

    added the project template

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# dat076
-booking system
+# Project Build with TypeORM
+        
+Steps to run this project:
+
+1. Run `npm i` command
+2. Setup database settings inside `ormconfig.json` file
+3. Run `npm start` command
---
+++ src/routes.ts
@@ -0,0 +1,32 @@
+import {UserController} from "./controller/UserController";
+import {TestController} from "./controller/TestController";
+
+
+export const Routes = [{
+    method: "get",
+    route: "/users",
+    controller: UserController,
+    action: "all"
+}, {
+    method: "get",
+    route: "/users/:id",
+    controller: UserController,
+    action: "one"
+}, {
+    method: "post",
+    route: "/users",
+    controller: UserController,
+    action: "save"
+}, {
+    method: "delete",
+    route: "/users",
+    controller: UserController,
+    action: "remove"
+}, {
+    method: "get",
+    route:"/tests",
+    controller: TestController,
+    action: "getAll"
+}
+
+];
